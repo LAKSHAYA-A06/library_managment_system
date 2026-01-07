f6f7209e21aa7ba0396cbe5d10e7a490847a2763
lakshaya <lakshaya.arul16@gmail.com>
2026-01-08 00:20:02 +0530

    Add login feature with simple authentication

---
+++ login.py
@@ -0,0 +1,8 @@
+# login.py
+def login(username, password):
+    if username == "admin" and password == "1234":
+        return "Login Successful"
+    else:
+        return "Invalid Credentials"
+
+print(login("admin", "1234"))
